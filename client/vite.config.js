7a42afa5316ba1c44d8b60449404f6e020a97d42
tzuhuangyen <912yan@gmail.com>
2024-03-06 14:57:24 +0700

    gh-page

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-// import ViteReactPlugin from "vite-plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,6 +7,9 @@ export default defineConfig({
   plugins: [react()],
 
   build: {
+    outDir: 'dist',
+    emptyOutDir: false, // 设置为 false，防止构建时清空输出目录
+
     rollupOptions: {
       input: 'src/main.jsx',
     },
