e69f4176a87fc85b9f750c69b0213eb032cbb857
tzuhuangyen <912yan@gmail.com>
2024-03-08 23:25:22 +0700

    page

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,17 +4,19 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/MERN-petslove/',
+  root: './client', // 设置根目录为 client 文件夹
   plugins: [react()],
 
   build: {
     outDir: 'dist',
     emptyOutDir: false, // 设置为 false，防止构建时清空输出目录
+    assetsDir: 'assets', // 将资源文件输出到名为 assets 的子目录中
 
     rollupOptions: {
-      input: 'src/main.jsx',
+      input: './src/main.jsx',
       output: {
-        entryFileNames: `assets/main.js`,
-        chunkFileNames: `assets/all.js`,
+        entryFileNames: `src/main.jxs`,
+        chunkFileNames: `src/all.js`,
         assetFileNames: `assets/[name].[ext]`,
       },
     },
