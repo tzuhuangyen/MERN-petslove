dba7ce93671bdda069002c6742cd22e7ace26ebd
tzuhuangyen <912yan@gmail.com>
2024-03-08 14:06:15 +0700

    page

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://tzuhuangyen.github.io/MERN-petslove/',
+  base: '/MERN-petslove/',
   plugins: [react()],
 
   build: {
